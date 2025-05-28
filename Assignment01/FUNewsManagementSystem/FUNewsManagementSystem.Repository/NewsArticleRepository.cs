d55b68643f14cde1d06919fdf041c6ad60961f9f
TriNHM <triminh0502@gmail.com>
2025-05-28 10:10:31 +0700

    [ADD] add assignment1 code

---
+++ Assignment01/FUNewsManagementSystem/FUNewsManagementSystem.Repository/NewsArticleRepository.cs
@@ -0,0 +1,22 @@
+﻿using FUNewsManagementSystem.Repository.Base;
+using FUNewsManagementSystem.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Repository
+{
+    public class NewsArticleRepository : GenericRepository<NewsArticle>
+    {
+        public NewsArticleRepository()
+        {
+            
+        }
+
+        public new async Task<List<NewsArticle>?> GetAll()        {            var itemList = await _context.NewsArticles                .Include(x => x.Tags)                 .OrderByDescending(x => x.CreatedDate)                .ToListAsync();            return itemList;        }
+        public async Task<List<NewsArticle>> Search(string? newsTitle, string? headline, string? newsContent, string? categoryName)        {            var itemList = await _context.NewsArticles                .Include(x => x.Tags)                .Include(x => x.Category)                .Where(x =>                    string.IsNullOrEmpty(newsTitle) || x.NewsTitle.Contains(newsTitle) ||                    string.IsNullOrEmpty(headline) || x.Headline.Contains(headline) ||                    string.IsNullOrEmpty(newsContent) || x.NewsContent.Contains(newsContent) ||                    string.IsNullOrEmpty(categoryName) || x.Category.CategoryName.Contains(categoryName))                .OrderByDescending(x => x.CreatedDate)                .ToListAsync();            return itemList;        }
+    }
+}
