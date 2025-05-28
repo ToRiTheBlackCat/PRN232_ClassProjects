8a1ebffd8e83da37c1a8e1443c3e2877c92b71c9
TriNHM <triminh0502@gmail.com>
2025-05-28 21:50:48 +0700

    [ADD] [ASIGN1] add remove with checking function

---
+++ Assignment01/FUNewsManagementSystem/FUNewsManagementSystem.Repository/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using FUNewsManagementSystem.Repository.Base;
+using FUNewsManagementSystem.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Repository
+{
+    public class AccountRepository : GenericRepository<SystemAccount>
+    {
+        public AccountRepository()
+        {
+
+        }
+        public async Task<bool> RemoveWithCheckingAsync(SystemAccount acc)
+        {
+            if (acc.NewsArticles == null)
+            {
+                _context.Remove(acc);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assignment01/FUNewsManagementSystem/FUNewsManagementSystem.Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using FUNewsManagementSystem.Repository.Base;
+using FUNewsManagementSystem.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUNewsManagementSystem.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>
+    {
+        public CategoryRepository()
+        {
+
+        }
+
+        public async Task<bool> RemoveWithCheckingAsync(Category category)
+        {
+            if (category.NewsArticles == null)
+            {
+                _context.Remove(category);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
