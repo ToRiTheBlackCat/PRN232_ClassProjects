5c5eb20cc5a86d6c329863a517875ba103e1383f
TriNHM <triminh0502@gmail.com>
2025-06-19 10:38:14 +0700

    [ADD] [ASSIGN2] add login page
    Not done

---
+++ Assignment02/FUNewsManagementSystem/FUNewsManagementSystem_FE.RazorPageWebApp/Constant/ConstantVariables.cs
@@ -0,0 +1,7 @@
+﻿namespace FUNewsManagementSystem_FE.RazorPageWebApp.Constant
+{
+    public class ConstantVariables
+    {
+        public static string APIEndPoint = "https://localhost:50013/api/";
+    }
+}
